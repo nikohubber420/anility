b97bd77da2906cc29941e97d2ed29c2113e50125
niko <nikobroking20111222@gmail.com>
2022-02-11 20:11:28 +0100

    first commit

---
+++ Anility UI/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Anility_UI
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.panel1.SuspendLayout();
+			this.groupBox1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+			this.panel1.Controls.Add(this.button3);
+			this.panel1.Controls.Add(this.button2);
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.progressBar1);
+			this.panel1.Location = new System.Drawing.Point(-1, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(605, 71);
+			this.panel1.TabIndex = 0;
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.BackColor = System.Drawing.Color.Yellow;
+			this.progressBar1.Location = new System.Drawing.Point(0, 12);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(605, 10);
+			this.progressBar1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.label1.Location = new System.Drawing.Point(13, 30);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(202, 31);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "A  N  I  L  I  T  Y";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+			this.button1.FlatAppearance.BorderSize = 0;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button1.Location = new System.Drawing.Point(241, 34);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(84, 26);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "R A G E";
+			this.button1.UseVisualStyleBackColor = false;
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+			this.button2.FlatAppearance.BorderSize = 0;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button2.Location = new System.Drawing.Point(341, 34);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(113, 26);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "V I S U A L S";
+			this.button2.UseVisualStyleBackColor = false;
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+			this.button3.FlatAppearance.BorderSize = 0;
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button3.Location = new System.Drawing.Point(469, 34);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(84, 26);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "M I S C";
+			this.button3.UseVisualStyleBackColor = false;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.label4);
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.groupBox1.Location = new System.Drawing.Point(13, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(155, 341);
+			this.groupBox1.TabIndex = 1;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "A I M   A S S I S T";
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.groupBox3);
+			this.panel2.Controls.Add(this.groupBox2);
+			this.panel2.Controls.Add(this.groupBox1);
+			this.panel2.Location = new System.Drawing.Point(12, 87);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(578, 372);
+			this.panel2.TabIndex = 1;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.groupBox2.Location = new System.Drawing.Point(191, 12);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(166, 341);
+			this.groupBox2.TabIndex = 2;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "A N T I - A I M";
+			this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.groupBox3.Location = new System.Drawing.Point(383, 12);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(166, 341);
+			this.groupBox3.TabIndex = 3;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "M I S C";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.label2.Location = new System.Drawing.Point(50, 39);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(61, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "A I M B O T";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.label3.Location = new System.Drawing.Point(50, 71);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(99, 13);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "T R I G G E R B O T";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.label4.Location = new System.Drawing.Point(50, 104);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(89, 13);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "B A CK T R A C K";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+			this.ClientSize = new System.Drawing.Size(602, 471);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.GroupBox groupBox3;
+	}
+}
+
